d5e7fdbce6ddf399ee20addaaf0ea7833c1767ff
Alessio Marucci <alessio.marucci@doxy.me>
2026-04-24 11:44:08 +0100

    update meilisearch version

--- src/meilisearchClient.js
+++ src/meilisearchClient.js
@@ -1,9 +1,9 @@
-import { MeiliSearch } from "meilisearch";
+import { Meilisearch } from "meilisearch";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const meilisearchClient = new MeiliSearch({
+const meilisearchClient = new Meilisearch({
   host: process.env.MEILISEARCH_HOST || "http://localhost:7700",
   apiKey: process.env.MEILISEARCH_API_KEY || "",
 });
